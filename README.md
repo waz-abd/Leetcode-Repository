606992400926602349df1d00704019694d6e96df
wasayabdul <wasay54@hotmail.com>
2025-10-31 01:00:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Leetcode-Repository
+
---
+++ Two-Sum/two-sum.py
@@ -0,0 +1,7 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        complement = target - nums[i]
+
+        for j in range(i + 1, len(nums)):
+            if nums[j] == complement:
+                return [i, j]
